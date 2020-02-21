b8c105ca658398ecb71904e6744f2649d160dac5
aaronwolf <aaron.wolf@gmail.com>
2020-02-21 13:36:17 +0200

    added home page & extended templates

--- wolfmath/urls.py
+++ wolfmath/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('projects/', include('projects.urls')),
     path('teach/', include('teach.urls')),
-    path('', include('views.home'))
+    path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
