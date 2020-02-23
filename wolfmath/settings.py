d4962892d567873413483f6757e893fa9b123809
aaronwolf <aaron.wolf@gmail.com>
2020-02-23 17:56:25 +0200

    added static images to home

--- wolfmath/settings.py
+++ wolfmath/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'teach',
     'projects',
     'bootstrap4',
-    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
