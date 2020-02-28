4d037ea601b97f91dc338ccff62061b2891e37b4
aaronwolf <aaron.wolf@gmail.com>
2020-02-28 16:04:04 +0200

    installed packages for jupyter notebook

--- wolfmath/settings.py
+++ wolfmath/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'teach',
     'projects',
     'bootstrap4',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -77,13 +78,17 @@ WSGI_APPLICATION = 'wolfmath.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# Install PostgreSQL: https://tutorial-extensions.djangogirls.org/en/optional_postgresql_installation/
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
