db9d8af30344364e312ef6c530a01f577c111072
aaronwolf <aaron.wolf@gmail.com>
2020-03-09 21:26:50 +0200

    set up views to make detail page in prokects

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
 def projects_list(request):
-  return render(request, 'projects/index.html')
+  projects = Project.objects.all()
+  return render(request, 'projects/index.html', {'projects': projects})
+
+def project_detail(request, pk):
+  project = Project.objects.get(pk=pk)
+  return render(request, 'projects/detail.html', {'project': project})
