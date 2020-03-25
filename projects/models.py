d674b529cb67dc661104133c0eb5bb5a1dc29bc8
aaronwolf <aaron.wolf@gmail.com>
2020-03-25 09:48:37 +0200

    Project Post styling

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
   title = models.CharField(max_length=100)
   description = models.TextField()
   technology = models.CharField(max_length=20)
-  image = models.ImageField(default='wolf-logo_D5.png', upload_to='project_pics')
+  image = models.ImageField(default="{% static '/img/wolf-logo_D5.png' %}", upload_to='project_pics')
   project_url = models.URLField(default="#")
   github_url = models.URLField(default="#")
   
